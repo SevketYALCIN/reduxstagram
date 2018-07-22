d897325d097688720b8c6c293dfb42992fa9eab5
Sevket <sevket.yalcin@outlook.com>
2018-07-22 17:37:17 +0900

    04 - Pre-redux app with simple routing

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,22 @@
-// let's go!
+import React from 'react'
+import { render } from 'react-dom'
+
+import Main from './components/main'
+import Single from './components/single'
+import PhotoGrid from './components/photogrid'
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+
+//import css
+import css from './styles/style.styl'
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}/>
+      <Route path="/view/:postId" component={Single}/>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'))
